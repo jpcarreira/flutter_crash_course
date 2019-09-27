c97428ab8904ae2b7ebb0d1f05f9d38511996bca
Joao Carreira <joaocarreira@me.com>
2019-09-27 23:27:56 +0100

    fix: overflow in location detail screen

--- lib/location_detail.dart
+++ lib/location_detail.dart
@@ -19,10 +19,12 @@ class LocationDetail extends StatelessWidget {
           style: Styles.navBarTitle,
         ),
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: _renderBody(context, location),
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: _renderBody(context, location),
+        ),
       ),
     );
   }

4d6e0acd5b208e691fa224ea637f51bdd84515c9
Joao Carreira <joaocarreira@me.com>
2019-09-28 23:55:34 +0100

    chore: added json_serializable, json_annotation and build_runner pkg; annotated model classes as @JsonSerializable and implemented static factory method to instantiate them using json

--- lib/models/location.dart
+++ lib/models/location.dart
@@ -1,10 +1,17 @@
+import "package:json_annotation/json_annotation.dart";
 import 'package:flutter_crash_course/models/location_fact.dart'
     show LocationFact;
 
+part 'location.g.dart';
+
+@JsonSerializable()
 class Location {
   final String name;
   final String url;
   final List<LocationFact> facts;
 
   Location({this.name, this.url, this.facts});
+
+  factory Location.fromJson(Map<String, dynamic> json) =>
+      _$LocationFromJson(json);
 }

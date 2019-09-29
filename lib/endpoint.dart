92143db1d3b52fc42ed6a734265a1e5e8b06ff13
Joao Carreira <joaocarreira@me.com>
2019-09-29 16:27:08 +0100

    feat: implemented API calls to get all locations and a location based on ID; added Endpoint utility class

---
+++ lib/endpoint.dart
@@ -0,0 +1,15 @@
+import "dart:core";
+
+class Endpoint {
+  static String apiScheme = "https";
+  static String apiHost = "fluttercrashcourse";
+  static String prefix = "/api/v1";
+
+  static Uri uri(String path, {Map<String, dynamic> queryParameters}) {
+    return Uri(
+        scheme: apiScheme,
+        host: apiHost,
+        path: '$prefix$path',
+        queryParameters: queryParameters);
+  }
+}
--- lib/models/location.dart
+++ lib/models/location.dart
@@ -1,17 +1,54 @@
 import "package:json_annotation/json_annotation.dart";
 import 'package:flutter_crash_course/models/location_fact.dart'
     show LocationFact;
+import "dart:async";
+import "dart:convert";
+import "package:http/http.dart" as http;
+import "package:flutter_crash_course/endpoint.dart";
 
 part 'location.g.dart';
 
 @JsonSerializable()
 class Location {
+  final int id;
   final String name;
   final String url;
   final List<LocationFact> facts;
 
-  Location({this.name, this.url, this.facts});
+  Location({this.id, this.name, this.url, this.facts});
 
   factory Location.fromJson(Map<String, dynamic> json) =>
       _$LocationFromJson(json);
+
+  static Future<List<Location>> fetchAll() async {
+    var uri = Endpoint.uri("/locations");
+
+    final response = await http.get(uri.toString());
+
+    if (response.statusCode != 200) {
+      throw response.body;
+    }
+
+    List<Location> locationsList = new List<Location>();
+
+    for (var locationItem in json.decode(response.body)) {
+      locationsList.add(Location.fromJson(locationItem));
+    }
+
+    return locationsList;
+  }
+
+  static Future<Location> fetchByID(String locationId) async {
+    var uri = Endpoint.uri("/locations/$locationId");
+
+    final response = await http.get(uri.toString());
+
+    if (response.statusCode != 200) {
+      throw response.body;
+    }
+
+    final Map<String, dynamic> locationJson = json.decode(response.body);
+
+    return Location.fromJson(locationJson);
+  }
 }
